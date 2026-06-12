2127bca040a0032ef3c9bf641aa711713987b538
Aman TS <amants2006@gmail.com>
2026-06-12 23:50:54 +0530

    working on more generalised image inputs

--- omr.py
+++ omr.py
@@ -1,68 +1,94 @@
 import cv2
 import utills
 
-img = cv2.imread("omr_answerd.jpg")
+img = cv2.imread("test_omr_1.jpeg")
+if img.shape[0] > 500 or img.shape[1] > 500:
+    img = cv2.resize(img, (500, 500))
 
 
 def main(image):
-
     if image is not None:
         # Pre-Processing
         grayed = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(grayed, (5, 5), 0)
-        edged = cv2.Canny(blurred, 100, 500)
+        thresh = cv2.threshold(
+            blurred, 150, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+        )[1]
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+        closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
+        edged = cv2.Canny(closed, 250, 400)
 
-        # countour detection
-        countours = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        countours = countours[0] if len(countours) == 2 else countours[1]
-        contour_data = utills.extractContourData(countours)
+        # contours detection
+        contours = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contour_data = utills.extractContourData(contours[0])
+        answers = utills.detectAnswerLayout(contour_data)
+        print(answers)
 
-        answer_layout = utills.detectAnswerLayout(contour_data)
-        if answer_layout:
-            answers = answer_layout[0]
-            x_min = answer_layout[1][0]
-            option_dist = answer_layout[2]
-            section_dist = answer_layout[3]
+        for data in contour_data:
+            x, y, w, h = data[1], data[2], data[3], data[4]
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imshow("image", image)
+        cv2.waitKey(0)
+
+        if answers is not None:
+            return answers
         else:
+            print("Failed to load answer layout")
             return -1
 
-        print("Section A")
-        for i in range(1, len(answers), 2):
-            x1, y1, w1, h1 = answers[i]
-
-            if x1 < x_min:
-                print("A")
-            elif x1 < (x_min + option_dist):
-                print("B")
-            elif x1 < (x_min + (2 * option_dist)):
-                print("C")
-            elif x1 < (x_min + (3 * option_dist)):
-                print("D")
-            else:
-                print("0")
-
-        print("Section B")
-        for i in range(0, len(answers), 2):
-            x2, y2, w2, h2 = answers[i]
-
-            if x2 < (x_min + section_dist + (3 * option_dist)):
-                print("A")
-            elif x2 < (x_min + section_dist + (4 * option_dist)):
-                print("B")
-            elif x2 < (x_min + section_dist + (5 * option_dist)):
-                print("C")
-            elif x2 < (x_min + section_dist + (6 * option_dist)):
-                print("D")
-            else:
-                print("0")
     else:
         print("Failed to load image")
         return -1
 
 
-main(img)
+def answer(answer_layout):
+    if len(answer_layout) == 4:
+        answers = answer_layout[0]
+        x_min = answer_layout[1]
+        option_dist = answer_layout[2]
+        section_dist = answer_layout[3]
+    else:
+        print("Failed to load answer layout")
+        return -1
+    section_a = []
+    section_b = []
 
-# For values of x we processed minimum value of x, the difference between two options and the difference between two sections...
-# Similarly we should do for y so that just by getting y_min and number of questions we should be able to get all y values that have options corresponding
-# The most natural response is to write a util function for both since most actions are repetative
-# Thus removing the ambiguity of unanswered questions
+    for i in range(0, len(answers), 2):
+        x1, y1, w1, h1 = answers[i]
+
+        if x1 < x_min:
+            section_a.append("A")
+        elif x1 < (x_min + option_dist):
+            section_a.append("B")
+        elif x1 < (x_min + (2 * option_dist)):
+            section_a.append("C")
+        elif x1 < (x_min + (3 * option_dist)):
+            section_a.append("D")
+        else:
+            section_a.append("0")
+
+    for i in range(1, len(answers), 2):
+        x2, y2, w2, h2 = answers[i]
+
+        if x2 < (x_min + section_dist + (3 * option_dist)):
+            section_b.append("A")
+        elif x2 < (x_min + section_dist + (4 * option_dist)):
+            section_b.append("B")
+        elif x2 < (x_min + section_dist + (5 * option_dist)):
+            section_b.append("C")
+        elif x2 < (x_min + section_dist + (6 * option_dist)):
+            section_b.append("D")
+        else:
+            section_b.append("0")
+
+    print(section_a + section_b)
+
+
+def answer_base(image):
+    # code
+    return -1
+
+
+answer_sheet = main(img)
+answered = answer(answer_sheet)
--- utills.py
+++ utills.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from statistics import median
+
 import cv2
 
 
@@ -10,35 +13,60 @@ def average(listOfItems):
 
 def extractContourData(contours):
     contourData = []
+    area_list = []
+    for contour in contours:
+        area_list.append(cv2.contourArea(contour))
+    avg_area = median(area_list)
+
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area > 10:
+        if 1.5 * avg_area < area:
             x, y, w, h = cv2.boundingRect(contour)
             perimeter = cv2.arcLength(contour, True)
             contourData.append([perimeter, x, y, w, h])
     return contourData
 
 
+def cluster_boundaries_y(ans):
+    ans = sorted(ans, key=lambda x: x[0])
+    y = [data[1] for data in ans]
+    x_count = defaultdict(int)
+    for s in y:
+        x_count[s] += 1
+    y_boundary = []
+    for key, value in x_count.items():
+        if value > 2:
+            y_boundary.append(key)
+    y_max = max(y_boundary)
+    y_min = (min(y_boundary) + y_max) // 2
+
+    if len(y_boundary) > 1:
+        return [y_min, y_max]
+    return None
+
+
 def detectAnswerLayout(contour_data):
     avg_perimeter = average([data[0] for data in contour_data])
-    y_avg = average([data[2] for data in contour_data])
-
     answers = []
     x_list = set()
-
+    cluster_y = cluster_boundaries_y(
+        [[data[1], data[2], data[3], data[4]] for data in contour_data]
+    )
+    print(cluster_y)
+    if cluster_y is not None:
+        y_min = cluster_y[0]
+        y_max = cluster_y[1]
+    else:
+        return None
     for data in contour_data:
-        if (
-            data[0] <= avg_perimeter
-            and data[2] >= y_avg - 70
-            and data[2] <= y_avg + 140
-        ):
+        if data[0] <= 1.1 * avg_perimeter and y_min <= data[2] < y_max:
             x, y, w, h = data[1], data[2], data[3], data[4]
             answers.append((x, y, w, h))
             x_list.add(x)
         else:
             continue
 
-    answers = sorted(answers, key=lambda x: x[1])
+    answers = sorted(answers, key=lambda x: (x[1], x[0]))
 
     x_list = sorted(x_list)
     x_min = x_list[0]
@@ -55,11 +83,27 @@ def detectAnswerLayout(contour_data):
         option_dist = sorted_x[0]
         section_dist = sorted_x[1]
         tolerence = option_dist // 2
-        x_min = [x_min + tolerence]
+        x_min = x_min + tolerence
     else:
         print("Error occured in option detection")
         return []
 
-    answerLayout = [answers, x_min, option_dist, section_dist]
+    answer_layout = [answers, x_min, option_dist, section_dist]
+    return answer_layout
 
-    return answerLayout
+
+def longest_continous_strip(contour_data):
+    data_strip = []
+    for data in contour_data:
+        data_strip.append([data[1], data[2]])
+    data_strip = sorted(data_strip, key=lambda x: x[0])
+    count = defaultdict(int)
+    for data in data_strip:
+        count[data[0]] += 1
+
+    max_x = 0
+    for key, value in count.items():
+        if value == max(count.values()):
+            max_x = key
+
+    return max_x
